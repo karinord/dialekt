ea4141798c840fdc528b253c23d26cbbcc030e1d
Kari Nordholm <kari.kj.no88@gmail.com>
2024-03-22 13:15:50 +0100

    added comments

--- dialekt/app.py
+++ dialekt/app.py
@@ -15,50 +15,26 @@ if "df" not in st.session_state:
 
 st.subheader("Ny registrering")
 
-#num_new_rows = st.sidebar.number_input("Add Rows",1,50)
-ncol = st.session_state.df.shape[1]  # col count
-rw = -1
+# Get the number of columns in the DataFrame
+number_of_columns = st.session_state.df.shape[1]
 
+# Initialize the index for the new row
+new_row_index = -1
+
+# Create a form for adding new data
 with st.form(key="add form", clear_on_submit= True):
-    cols = st.columns(ncol)
-    rwdta = []
+    cols = st.columns(number_of_columns)
+    new_row = []
 
-    for i in range(ncol):
-        rwdta.append(cols[i].text_input(st.session_state.df.columns[i]))
-
-    # you can insert code for a list comprehension here to change the data (rwdta) 
-    # values into integer / float, if required
+    for i in range(number_of_columns):
+        new_row.append(cols[i].text_input(st.session_state.df.columns[i]))
 
     if st.form_submit_button("Legg til"):
-            rw = st.session_state.df.shape[0] + 1
-            st.session_state.df.loc[rw] = rwdta
+            new_row_index = st.session_state.df.shape[0] + 1
+            st.session_state.df.loc[new_row_index] = new_row
 
 st.subheader("Oversikt over poster")
 
-#st.dataframe(st.session_state.df)
 edited_df = st.data_editor(st.session_state.df, num_rows="dynamic")
 
 
-
-"""import streamlit as st
-import pandas as pd
-
- 
-
-
-
-df = pd.DataFrame(
-    [
-       {" ": "et", "Dialektord": "kådn", "Betydning": "korn", "Bøying": "kådn-kåddne-kådn-kåddne", "Bruk": "", "Merknader": "test", "tvil": True, "Endring": "rn -> dn"},
-       {" ": "et", "Dialektord": "kådn", "Betydning": "korn", "Bøying": "kådn-kåddne-kådn-kåddne", "Bruk": "", "Merknader": "test", "tvil": True, "Endring": "rn -> dn"},
-       {" ": "et", "Dialektord": "kådn", "Betydning": "korn", "Bøying": "kådn-kåddne-kådn-kåddne", "Bruk": "", "Merknader": "test", "tvil": True, "Endring": "rn -> dn"},
-   ]
-)
-
-edited_df = st.data_editor(df, num_rows="dynamic")
-"""
- 
-
-#favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-
-#st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
